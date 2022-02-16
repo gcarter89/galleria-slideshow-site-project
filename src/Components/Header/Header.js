22b91df24b22c15b79edf5ab78285eb6356f0955
Gareth Carter <gareth.carter@outlook.com>
2022-02-16 23:55:30 +0000

    initial commit of assets and structure

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+import styles from './Header.module.scss';
+import Logo from '../../Assets/shared/logo.svg';
+
+export function Header() {
+    const [isStart, setStart] = useState(false);
+
+    function handleHeaderButtonClick(event) {
+        event.preventDefault();
+        setStart(!isStart);
+    }
+
+    return (
+        <header className={styles.header}>
+            <img className={styles.header__logo} src={Logo} alt='Galleria logo' />
+            <button className={styles.header__button} onClick={(e) => handleHeaderButtonClick(e)}>{isStart ? 'Stop Slideshow' : 'Start Slideshow'}</button>
+        </header>
+    )
+}

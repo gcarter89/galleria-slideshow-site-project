ab12eb81835ae65fad3172bd2c8f6340426aff4e
Gareth Carter <gareth.carter@outlook.com>
2022-02-25 01:13:26 +0000

    added button functionality

--- src/App.js
+++ src/App.js
@@ -11,11 +11,13 @@ function App() {
     const [selectedArtwork, setSelectedArtwork] = useState(data[0]);
     const [artworkIndex, setArtworkIndex] = useState(0);
 
+
+
     const [viewImage, setViewImage] = useState(false);
 
 
     const progressPercentage = ((artworkIndex + 1) / data.length) * 100;
-
+    const dataLength = data.length - 1;
 
     function handleArtworkSelect(event, artwork) {
         event.preventDefault();
@@ -31,6 +33,27 @@ function App() {
         setArtworkIndex(0);
         setStart(!isStart);
     }
+
+    // function handleForwardButton(event) {
+    //     event.preventDefault();
+    //     if (artworkIndex === data.length - 1 ) {
+    //         return setEndOfSeries(true);
+    //     }
+    //     const nextIndex = artworkIndex + 1;
+    //     setArtworkIndex(nextIndex);
+    //     return setEndOfSeries(false);
+    // }
+
+    // function handleBackButton(event) {
+    //     event.preventDefault();
+    //     if (artworkIndex === 0) {
+    //         return setEndOfSeries(true);
+    //     }
+        
+    //     const previousIndex = artworkIndex - 1;
+    //     setArtworkIndex(previousIndex);
+    //     return setEndOfSeries(false);
+    // }
 
     const handleArtworkChange = useCallback(() => {
         const updatedIndex = artworkIndex + 1;
@@ -52,7 +75,7 @@ function App() {
         <>
             <Header handleHeaderClick={handleHeaderClick} isStart={isStart}/>
             {isStart ? <Detail viewImage={viewImage} setViewImage={setViewImage} handleArtworkChange={handleArtworkChange} selectedArtwork={selectedArtwork} /> : <Masonry data={data} handleArtworkSelect={handleArtworkSelect}/>}
-            {isStart ? <Footer progressPercentage={progressPercentage} selectedArtwork={selectedArtwork} setSelectedArtwork={setSelectedArtwork} /> : null}
+            {isStart ? <Footer dataLength={dataLength} progressPercentage={progressPercentage} selectedArtwork={selectedArtwork} artworkIndex={artworkIndex} setArtworkIndex={setArtworkIndex} /> : null}
         </>
     );
 }
--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,13 +1,50 @@
 import styles from './Footer.module.scss';
 import buttonBackImage from '../../assets/shared/icon-back-button.svg';
 import buttonForwardImage from '../../assets/shared/icon-next-button.svg';
+import { useEffect, useState } from 'react';
 
 
-export function Footer({selectedArtwork, progressPercentage}) {
+export function Footer({selectedArtwork, progressPercentage, setArtworkIndex, artworkIndex, dataLength}) {
+
+    const [endOfSeries, setEndOfSeries] = useState(false);
+
+    const [startOfSeries, setStartOfSeries] = useState(false);
+
+
+    useEffect(() => {
+        if (artworkIndex === dataLength) {
+            setStartOfSeries(false);
+            return setEndOfSeries(true);
+        }
+
+        if (artworkIndex === 0) {
+            setEndOfSeries(false);
+            return setStartOfSeries(true);
+        }
+
+        setStartOfSeries(false);
+        setEndOfSeries(false);
+
+
+    },[artworkIndex, dataLength])
 
     const progressStyles = {
         width: `${progressPercentage}%`
     }
+
+    function handleBackButton(event) {
+        event.preventDefault()
+        const prevIndex = artworkIndex - 1;
+        setArtworkIndex(prevIndex);
+    }
+
+    function handleForwardButton(event) {
+        event.preventDefault();
+        const nextIndex = artworkIndex + 1;
+        setArtworkIndex(nextIndex);
+
+    }
+
 
     return (
     <footer className={styles.footer}>
@@ -17,12 +54,25 @@ export function Footer({selectedArtwork, progressPercentage}) {
             <h2 className={styles.footer__artistName}>{selectedArtwork.artist.name}</h2>
         </div>
         <div className={styles.footer__artworkButtons}>
-            <button className={styles.footer__buttonBack}>
-                <img src={buttonBackImage} alt="button back" />
-            </button>
-            <button className={styles.footer__buttonForward}>
-                <img src={buttonForwardImage} alt="button forward" />
-            </button>
+            {startOfSeries ? 
+                <button>
+                    <img src={buttonBackImage} className={styles.footer__buttonBack + ' ' + styles._end}  alt="button back" />
+                </button>
+            :
+                <button onClick={(e) => handleBackButton(e)} className={styles.footer__buttonBack}>
+                    <img src={buttonBackImage} alt="button back" />
+                </button>
+            }
+
+            {endOfSeries ?
+                <button className={styles.footer__buttonForward + ' ' + styles._end}>
+                    <img src={buttonForwardImage} alt="button forward" />
+                </button>
+            :
+                <button onClick={(e) => handleForwardButton(e)} className={styles.footer__buttonForward}>
+                    <img src={buttonForwardImage} alt="button forward" />
+                </button>
+            }
         </div>
 
     </footer>
